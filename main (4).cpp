e817a7e4d301d9871c690debe6319190bbf20af9
HumbertoMondragon <142269112+HumbertoMondragon@users.noreply.github.com>
2024-06-07 08:25:14 -0600

    cambio al main

--- main (4).cpp
+++ main (4).cpp
@@ -1,122 +1,30 @@
+#include "Biblioteca.h"
+#include "Derivados.h"
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-// Clase madre Libro
-class Libro {
-protected:
-    string titulo;
-    int id;
-    bool disponible;
-
-public:
-    Libro(string t, int i) : titulo(t), id(i), disponible(true) {}
-
-    void mostrar() {  
-        cout << "Título: " << titulo << "\nID: " << id << "\nDisponible: " << (disponible ? "Sí" : "No") << endl;
-    }
-
-    void prestar() { 
-        if (disponible) {
-            disponible = false;
-            cout << "Libro prestado." << endl;
-        } else {
-            cout << "El libro no está disponible." << endl;
-        }
-    }
-
-    void devolver() {  
-        disponible = true;
-        cout << "Libro devuelto." << endl;
-    }
-
-    // Función mostrar con parámetro adicional
-    void mostrar(bool detalle) {
-        if (detalle) {
-            cout << "Título: " << titulo << "\nID: " << id << "\nDisponible: " << (disponible ? "Sí" : "No") << endl;
-        } else {
-            cout << "Título: " << titulo << "\nID: " << id << endl;
-        }
-    }
-};
-
-// Clase hija Novela
-class Novela : public Libro {
-private:
-    string autor;
-
-public:
-    Novela(string t, int i, string a) : Libro(t, i), autor(a) {}
-
-    void mostrar() {  // Sobrescritura 
-        Libro::mostrar();
-        cout << "Autor: " << autor << endl;
-    }
-
-    void mostrar(bool detalle) {  // Sobrecarga
-        if (detalle) {
-            Libro::mostrar();
-            cout << "Autor: " << autor << endl;
-        } else {
-            cout << "Título: " << titulo << "\nID: " << id << endl;
-        }
-    }
-
-    void prestar() {  // Sobrescritura 
-        if (disponible) {
-            disponible = false;
-            cout << "La novela \"" << titulo << "\" ha sido prestada." << endl;
-        } else {
-            cout << "La novela \"" << titulo << "\" no está disponible." << endl;
-        }
-    }
-};
-
-// Clase hija Poesia
-class Poesia : public Libro {
-private:
-    string autor;
-
-public:
-    Poesia(string t, int i, string a) : Libro(t, i), autor(a) {}
-
-    void mostrar() {  // Sobrescritura 
-        Libro::mostrar();
-        cout << "Autor: " << autor << endl;
-    }
-
-    void mostrar(bool detalle) {  // Sobrecarga
-        if (detalle) {
-            Libro::mostrar();
-            cout << "Autor: " << autor << endl;
-        } else {
-            cout << "Título: " << titulo << "\nID: " << id << endl;
-        }
-    }
-
-    void prestar() {  // Sobrescritura
-        if (disponible) {
-            disponible = false;
-            cout << "El libro de poesía \"" << titulo << "\" ha sido prestado." << endl;
-        } else {
-            cout << "El libro de poesía \"" << titulo << "\" no está disponible." << endl;
-        }
-    }
-};
-
 int main() {
-    Novela n("Don Quijote", 1, "Miguel de Cervantes");
-    Poesia p("Veinte poemas de amor", 2, "Pablo Neruda");
+    Biblioteca biblioteca;
 
-    n.prestar();  
-    p.prestar();  
+    biblioteca.agregarItem(new Revista("National Geographic", "Varios", 1));
+    biblioteca.agregarItem(new Libro("Cien Años de Soledad", "Gabriel García Márquez", 2));
+    biblioteca.agregarItem(new Comic("Spider-Man", "Stan Lee", 3));
 
-    n.mostrar();  
-    p.mostrar(); 
+    biblioteca.mostrarItems();
 
-    
-    n.mostrar(false);  
-    p.mostrar(true);   
+    if (biblioteca.rentarItem(2)) {
+        cout << "El libro con ID 2 ha sido rentado.\n";
+    } else {
+        cout << "El libro con ID 2 no está disponible para renta.\n";
+    }
+
+    biblioteca.mostrarItems();
+
+    biblioteca.devolverItem(2);
+    cout << "El libro con ID 2 ha sido devuelto.\n";
+
+    biblioteca.mostrarItems();
 
     return 0;
 }
