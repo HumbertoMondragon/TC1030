6dd9127f25c74af6477995cc851977706b9d9481
HumbertoMondragon <142269112+HumbertoMondragon@users.noreply.github.com>
2024-06-07 10:55:33 -0600

    Update item.h

--- item.h
+++ item.h
@@ -14,11 +14,30 @@ protected:
 
 public:
     Item(const string& t, const string& a, int i);
-    virtual void mostrarInfo() const = 0; // Método puro virtual
+    virtual void mostrarInfo() const = 0; 
     bool estaRentado() const;
     void rentar();
     void devolver();
     int getID() const;
 };
+
+Item::Item(const string& t, const string& a, int i)
+    : titulo(t), autor(a), id(i), isRented(false) {}
+
+bool Item::estaRentado() const {
+    return isRented;
+}
+
+void Item::rentar() {
+    isRented = true;
+}
+
+void Item::devolver() {
+    isRented = false;
+}
+
+int Item::getID() const {
+    return id;
+}
 
 #endif // ITEM_H
