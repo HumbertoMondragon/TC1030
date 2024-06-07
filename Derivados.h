f89b316b9af9bd3d78edacc391104a2badcf2a59
HumbertoMondragon <142269112+HumbertoMondragon@users.noreply.github.com>
2024-06-07 10:55:57 -0600

    Update Derivados.h

--- Derivados.h
+++ Derivados.h
@@ -1,5 +1,6 @@
 #ifndef DERIVADOS_H
 #define DERIVADOS_H
+#include <iostream>
 
 #include "Item.h"
 
@@ -23,4 +24,32 @@ public:
     void mostrarInfo() const override;
 };
 
+
+
+// Implementación de Revista
+Revista::Revista(const string& t, const string& a, int i)
+    : Item(t, a, i) {}
+
+void Revista::mostrarInfo() const {
+    cout << "Revista\n";
+    cout << "Titulo: " << titulo << "\nAutor: " << autor << "\nID: " << id << "\n";
+}
+
+
+Libro::Libro(const string& t, const string& a, int i)
+    : Item(t, a, i) {}
+
+void Libro::mostrarInfo() const {
+    cout << "Libro\n";
+    cout << "Titulo: " << titulo << "\nAutor: " << autor << "\nID: " << id << "\n";
+}
+
+// Implementación de Comic
+Comic::Comic(const string& t, const string& a, int i)
+    : Item(t, a, i) {}
+
+void Comic::mostrarInfo() const {
+    cout << "Comic\n";
+    cout << "Titulo: " << titulo << "\nAutor: " << autor << "\nID: " << id << "\n";
+}
 #endif // DERIVADOS_H
