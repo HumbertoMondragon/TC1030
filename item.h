35bfb52bc2f1bb244530f400205bc6994645060a
HumbertoMondragon <142269112+HumbertoMondragon@users.noreply.github.com>
2024-06-14 00:26:19 -0600

    Update item.h
    Se agregan comments

--- item.h
+++ item.h
@@ -1,3 +1,15 @@
+/*
+ *
+ * Proyecto Biblioteca clase abstracta Item
+ * Humberto Mondragón García
+ * A01711912
+ * 14/06/2024
+ * Esta clase define un objeto abstracto de tipo Item que contiene las propiedades
+ * y métodos comunes a todos los artículos en la biblioteca, como título, autor, ID,
+ * y estado de renta. Esta clase es utilizada como base para las clases derivadas 
+ * Revista, Libro y Comic.
+ */
+
 #ifndef ITEM_H
 #define ITEM_H
 
@@ -21,21 +33,63 @@ public:
     int getID() const;
 };
 
+/**
+ * Constructor Item
+ *
+ * Inicializa un artículo con título, autor, ID y establece su estado de renta en false.
+ *
+ * @param string t, título del artículo
+ * @param string a, autor del artículo
+ * @param int i, ID del artículo
+ * @return
+ */
 Item::Item(const string& t, const string& a, int i)
     : titulo(t), autor(a), id(i), isRented(false) {}
 
+/**
+ * estaRentado verifica si el artículo está rentado
+ *
+ * Retorna true si el artículo está rentado, false en caso contrario.
+ *
+ * @param
+ * @return bool, estado de renta del artículo
+ */
 bool Item::estaRentado() const {
     return isRented;
 }
 
+/**
+ * rentar marca el artículo como rentado
+ *
+ * Establece el estado de renta del artículo en true.
+ *
+ * @param
+ * @return
+ */
 void Item::rentar() {
     isRented = true;
 }
 
+/**
+ * devolver marca el artículo como disponible
+ *
+ * Establece el estado de renta del artículo en false.
+ *
+ * @param
+ * @return
+ */
 void Item::devolver() {
     isRented = false;
 }
 
+/**
+ * getID obtiene el ID del artículo
+ *
+ * Retorna el ID del artículo.
+ *
+ * @param
+ * @return int, ID del artículo
+ */
 int Item::getID() const {
     return id;
 }
